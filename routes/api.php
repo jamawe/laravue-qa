d10434f761da6fc2d34b25c1c814cf3c89b6fa6b
Jana Wernick <jana.wernick@gmail.com>
2021-09-14 17:58:20 +0200

    Test for fetching the authenticated user added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserQuestionController;
+use App\Http\Controllers\AuthUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
     // Any route inside this callback will be protected by the api
+
+    Route::get('/auth-user', [AuthUserController::class, 'show']);
 
     Route::apiResources([
         '/questions' => QuestionController::class,
