523d1be9f2ad78c02af6f6536419a49f762890e1
Jana Wernick <jana.wernick@gmail.com>
2021-10-03 14:17:06 +0200

    A user can view a single question with answers test

--- tests/Feature/QuestionAnswerTest.php
+++ tests/Feature/QuestionAnswerTest.php
@@ -144,9 +144,62 @@ class AnswerTest extends TestCase
             ]);
     }
 
+    /** @test */
+    public function a_user_can_view_a_single_question_with_answers()
+    {
+        $this->actingAs($user = User::factory()->create(), 'api');
+        $question = Question::factory()->create(['id' => 123, 'user_id' => $user->id]);
+
+        // dd($question);
+
+        $this->json('post', '/api/questions/'.$question->id.'/answer', [
+            'body' => 'A great answer!',
+        ]);
+
+        $response = $this->get('/api/questions/'.$question->id);
+
+        $answer = Answer::first();
+
+        // dd($response);
+        $response->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'type' => 'questions',
+                    'attributes' => [
+                        'answers' => [
+                            'data' => [
+                                [
+                                    'data' => [
+                                        'type' => 'answers',
+                                        'answer_id' => 1,
+                                        'attributes' => [
+                                            'answered_by' => [
+                                                'data' => [
+                                                    'user_id' => $user->id,
+                                                    'attributes' => [
+                                                        'name' => $user->name,                                        
+                                                    ]
+                                                ]
+                                            ],
+                                            'body' => 'A great answer!',
+                                            'answered_at' => $answer->created_at->diffForHumans(),
+                                        ]
+                                    ],
+                                    'links' => [
+                                        'self' => url('/questions/123'),
+                                    ]
+                                ]
+                            ],
+                            'answer_count' => 1,
+                        ]
+                    ]
+                ]
+            ]);
+    }
+
     // a_question_is_returned_with_answers
 
-    // a_user_can_view_a_single_question_with_answers
+    // 
 
 
 }
